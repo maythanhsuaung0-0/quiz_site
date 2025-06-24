2c5b4ecf16a93234f58eb72500cf5860815d838d
wenxiong342@gmail.com <wenxiong342@gmail.com>
2025-06-24 14:06:31 -0400

    Update Result Page

--- answer.js
+++ answer.js
@@ -9,6 +9,7 @@ questionList.forEach(e => {
     let addin = `<div class="answerBoard"><div class="question">${question}</div><div class="answers">${answer}</div></div>`
 
     document.querySelector("br").insertAdjacentHTML("afterend", addin);
+
 })
 
 //Change the status bar in the result page.
@@ -17,6 +18,15 @@ function statusBar(correct, total){
     let correctPercentage = Math.floor((correct/total)*100);
     document.documentElement.style.setProperty("--status-bar", `${correctPercentage}%`);
     document.querySelector("div#statusBar div").innerHTML= `${correctPercentage}%`;
+
+    messageBar(correctPercentage)
 }
 
-statusBar(50,100)
+function messageBar(score){
+
+    if (score < 50){
+        document.querySelector("div#message").innerHTML = "Try Hard Next Time";
+    }
+}
+
+statusBar(50,100);
